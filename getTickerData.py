f075ac64d9f4561c2f5a29e90be284c1e62afae0
Rongtian Yue <50688769+rongtianyue@users.noreply.github.com>
2020-01-21 14:40:41 -0500

    Add files via upload

--- getTickerData.py
+++ getTickerData.py
@@ -2,9 +2,10 @@ from openpyxl import Workbook
 import requests
 import time
 
+
 def main():
 
-    limit = 16000 #under 1.5s per ticker point average
+    limit = 16000  # under 1.5s per ticker point average
 
     for y in range(2018, 2020):
         year = str(y)
@@ -21,26 +22,30 @@ def main():
                     day = "0{}".format(d)
                 else:
                     day = str(d)
-                date = "{}-{}-{}".format(year, month, day) #format: yyyy-mm-dd, can't have single digit for month or day
-                url = "https://api.polygon.io/v2/ticks/stocks/nbbo/DG/{}?limit={}&apiKey={redactedAPIKey}".format(date, limit) #for DG
+                date = "{}-{}-{}".format(year, month, day)
+                # format: yyyy-mm-dd can't have single digit for month or day
+                url = "https://api.polygon.io/v2/ticks/stocks/nbbo/DG/" \
+                      "{}?limit={}&apiKey={redactedAPIKEY}".format(date, limit)
                 mfile = requests.get(url).json()
                 wb = Workbook()
                 ws = wb.active
                 for i in range(0, limit):
                     try:
-                        print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(((mfile['results'][i]['t']) / 1000000000))))
+                        print(time.strftime('%Y-%m-%d %H:%M:%S',
+                                            time.localtime(((mfile['results'][i]['t']) / 1000000000))))
                     except KeyError:
                         continue
                     a.append(time.strftime('%Y-%m-%d %H:%M:%S',
-                                               time.localtime(((mfile['results'][i]['t']) / 1000000000))))
+                                           time.localtime(((mfile['results'][i]['t']) / 1000000000))))
                     b.append(mfile['results'][i]['p'])
                     c.append(mfile['results'][i]['P'])
 
                 ws.append(a)
                 ws.append(b)
                 ws.append(c)
-                saveas = "DG-{}-prices.xlsx".format(date)
-                wb.save(saveas)
+                saver = "DG-{}-prices.xlsx".format(date)
+                wb.save(saver)
+
 
 if __name__ == '__main__':
-        main()
+    main()
